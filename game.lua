9f1353e5a599e6bbcccc056d40fe5247c9b0a984
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2014-11-16 09:57:49 +1300

    work around a bug called Windows

--- game.lua
+++ game.lua
@@ -57,25 +57,26 @@ function on_tick(sec_current, sec_delta)
 			opos[i][5], opos[i][6], opos[i][7])
 	end
 	
-	local c1 = level_get(obx, obz)
+	-- math.floor necessary because Windows sucks
+	local c1 = level_get(math.floor(obx), math.floor(obz))
 
 	spd = 2.0
 	nobx = obx + obvx*sec_delta*spd
 	nobz = obz + obvz*sec_delta*spd
 
-	local c2 = level_get(nobx + obvx*0.5, nobz + obvz*0.5)
+	local c2 = level_get(math.floor(nobx + obvx*0.5), math.floor(nobz + obvz*0.5))
 
 	if c1 ~= c2 then
 		if c2 == "." or ((c1 == "#" or c1 == "&") and c2 == '"') then
 			obvx, obvz = obvz, -obvx
 			nobx = obx + obvx*sec_delta*spd
 			nobz = obz + obvz*sec_delta*spd
-			c2 = level_get(nobx + obvx*0.5, nobz + obvz*0.5)
+			c2 = level_get(math.floor(nobx + obvx*0.5), math.floor(nobz + obvz*0.5))
 			if c2 == "." or ((c1 == "#" or c1 == "&") and c2 == '"') then
 				obvx, obvz = -obvx, -obvz
 				nobx = obx + obvx*sec_delta*spd
 				nobz = obz + obvz*sec_delta*spd
-				c2 = level_get(nobx + obvx*0.5, nobz + obvz*0.5)
+				c2 = level_get(math.floor(nobx + obvx*0.5), math.floor(nobz + obvz*0.5))
 			end
 		end
 	end
